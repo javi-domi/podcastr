d3b6401348030f13a5d669ea92fa2dd681e6316f
Javier Dominguez <javdominguez@icloud.com>
2021-04-29 19:00:49 -0300

    Controles y seleccion de podcast

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Podcastr
+
+Podcast app desenvolvido no NLW da Rocketseat
+
+## Aprendizado e tecnologias utilizadas
+
+- React
+  - Hooks
+  - Context API
+- NextJS
+- Sass
+- TypeScript
--- podcastr-app/src/pages/index.tsx
+++ podcastr-app/src/pages/index.tsx
@@ -6,7 +6,8 @@ import ptBR from "date-fns/locale/pt-BR";
 import { api } from "../services/api";
 import { convertDuration } from "../utils/convertDuration";
 import styles from "./home.module.scss";
-import React from "react";
+import React, { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 type Episode = {
   id: string;
@@ -24,6 +25,7 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisode}>
@@ -51,7 +53,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episodio" />
                 </button>
               </li>
